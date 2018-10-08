f4b74d8447e3ea7b95a9888de1101bff78143c29
Stephane Gosselin <stephane.crypto@gmail.com>
2018-10-08 11:13:41 -0700

    compile errors

--- contracts/Wrapped725.sol
+++ contracts/Wrapped725.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.24;
 
 import "./ERC725.sol";
-import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721BasicToken.sol";
 
-contract Wrapped725 is ERC721 {
+contract Wrapped725 is ERC721BasicToken {
 
     mapping(uint256 => address) nftToIdentity;
 
@@ -36,14 +36,14 @@ contract Wrapped725 is ERC721 {
         _mint(msg.sender, token);
     }
 
-    function burnIdentityToken(uint256 _token, address _newOwner) public onlyTokenHolder {
+    function burnIdentityToken(uint256 _token, address _newOwner) public onlyTokenHolder(_token) {
         ERC725 identity = ERC725(nftToIdentity[_token]);
 
         // check for permission
         require(identity.keyHasPurpose(KEY,1));
 
         // add permission
-        require(identity.addKey(keccak256(_newOwner),1));
+        require(identity.addKey(keccak256(_newOwner),1,1));
 
         // burn721
         delete nftToIdentity[_token];
@@ -52,21 +52,21 @@ contract Wrapped725 is ERC721 {
 
     /// 725 Functions ///
 
-    function addKey(uint256 _token, bytes32 _key, uint256 _purpose, uint256 _keyType) public onlyTokenHolder returns (bool success) {
+    function addKey(uint256 _token, bytes32 _key, uint256 _purpose, uint256 _keyType) public onlyTokenHolder(_token) returns (bool success) {
         require(_purpose != 1);
         return ERC725(nftToIdentity[_token]).addKey(_key, _purpose, _keyType);
     }
 
-    function removeKey(uint256 _token, bytes32 _key, uint256 _purpose) public onlyTokenHolder returns (bool success) {
+    function removeKey(uint256 _token, bytes32 _key, uint256 _purpose) public onlyTokenHolder(_token) returns (bool success) {
         require(_purpose != 1);
         return ERC725(nftToIdentity[_token]).removeKey(_key, _purpose);
     }
 
-    function execute(uint256 _token, address _to, uint256 _value, bytes _data) public onlyTokenHolder returns (uint256 executionId) {
+    function execute(uint256 _token, address _to, uint256 _value, bytes _data) public onlyTokenHolder(_token) returns (uint256 executionId) {
         return ERC725(nftToIdentity[_token]).execute(_to, _value, _data);
     }
 
-    function approve(uint256 _token, uint256 _id, bool _approve) public onlyTokenHolder returns (bool success) {
+    function approve(uint256 _token, uint256 _id, bool _approve) public onlyTokenHolder(_token) returns (bool success) {
         return ERC725(nftToIdentity[_token]).approve(_id, _approve);
     }
 
